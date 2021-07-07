1b8f88bd0450f05d99697b77aa2847dc6b6ddd81
v-king777 <v.kolpashchikov@gmail.com>
2021-07-07 14:34:45 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/BLL/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Models
+{
+    public class User
+    {
+        public User(
+            int id,
+            string firstName,
+            string lastName,
+            string password,
+            string email,
+            string photo,
+            string favoriteMovie,
+            string favoriteBook,
+            IEnumerable<Message> incomingMessages,
+            IEnumerable<Message> outgoingMessages)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            Email = email;
+            Photo = photo;
+            FavoriteMovie = favoriteMovie;
+            FavoriteBook = favoriteBook;
+            IncomingMessages = incomingMessages;
+            OutgoingMessages = outgoingMessages;
+        }
+
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+        public string FavoriteMovie { get; set; }
+        public string FavoriteBook { get; set; }
+        public IEnumerable<Message> IncomingMessages { get; }
+        public IEnumerable<Message> OutgoingMessages { get; }
+    }
+}
